c3f77ed83b8251f6a31117d0ae51c840d355dfca
Slava Monich <slava.monich@jolla.com>
2020-01-07 17:05:01 +0200

    [test] Added test for combined Internet+MMS provisioning

--- test/test-decoder/test-decoder.c
+++ test/test-decoder/test-decoder.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015-2017 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2015-2020 Jolla Ltd.
+ * Copyright (C) 2015-2020 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of BSD license as follows:
  *
@@ -13,9 +13,9 @@
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of Jolla Ltd nor the names of its contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -169,6 +169,11 @@ static const struct provisioning_data prov_moi_2 = {
 	.mms = &prov_moi_mms
 };
 
+static const struct provisioning_data prov_moi_3 = {
+	.internet = &prov_moi_internet,
+	.mms = &prov_moi_mms
+};
+
 /* ======== Beeline ======== */
 
 static struct provisioning_mms prov_beeline_mms = {
@@ -192,6 +197,7 @@ static const struct test_decoder_data tests [] = {
 	{ TEST_PREFIX "dna_2", "prov_dna_2.wbxml", &prov_dna_2 },
 	{ TEST_PREFIX "moi_1", "prov_moi_1.wbxml", &prov_moi_1 },
 	{ TEST_PREFIX "moi_2", "prov_moi_2.wbxml", &prov_moi_2 },
+	{ TEST_PREFIX "moi_3", "prov_moi_3.wbxml", &prov_moi_3 },
 	{ TEST_PREFIX "beeline_2", "prov_beeline_2.wbxml", &prov_beeline_2 }
 };
 
